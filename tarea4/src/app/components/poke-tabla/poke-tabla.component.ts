937384ac5b440369a934d1a78dc821c42e9baaf7
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2021-05-07 13:49:09 -0500

    api consumida, array de pokemones creado

--- tarea4/src/app/components/poke-tabla/poke-tabla.component.ts
+++ tarea4/src/app/components/poke-tabla/poke-tabla.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Subscriber } from 'rxjs';
 @Component({
   selector: 'poke-tabla',
   templateUrl: './poke-tabla.component.html',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeTablaComponent implements OnInit {
 
-  constructor() { }
+  pokeList = []
 
-  ngOnInit() {}
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/`)
+    .subscribe(res => {
+        console.log(res);
+        this.pokeList = res.results;
+        console.log(this.pokeList)
+    })
+  }
 
 }
